50623c2159737201a92e824dbe1ebf4305576c2c
Ricardo Pereira <ricardopereira@whitesmith.co>
2020-01-17 11:28:42 +0000

    Add the new '-Wunguarded-availability' clang’s diagnostic  that warns about uses of new APIs that were introduced in a system  whose version is newer than the deployment target version.

--- SocketRocket/Internal/Utilities/SRURLUtilities.m
+++ SocketRocket/Internal/Utilities/SRURLUtilities.m
@@ -75,7 +75,9 @@ extern NSString *_Nullable SRStreamNetworkServiceTypeFromURLRequest(NSURLRequest
             break;
 #if (__MAC_OS_X_VERSION_MAX_ALLOWED >= 101200 || __IPHONE_OS_VERSION_MAX_ALLOWED >= 100000 || __TV_OS_VERSION_MAX_ALLOWED >= 100000 || __WATCH_OS_VERSION_MAX_ALLOWED >= 30000)
         case NSURLNetworkServiceTypeCallSignaling:
-            networkServiceType = NSStreamNetworkServiceTypeCallSignaling;
+            if (@available(macOS 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)) {
+                networkServiceType = NSStreamNetworkServiceTypeCallSignaling;
+            }
             break;
 #endif
     }
